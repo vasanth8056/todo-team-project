7784abfed4e133172e370635e80f18fdfc5a23b4
vasanth8056 <manovasanthmrva7723@gmail.com>
2025-08-01 12:05:13 +0530

    Add new file ToDo application

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return;
+  
+    const li = document.createElement("li");
+    li.textContent = taskText;
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = () => li.remove();
+  
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+  
+    taskInput.value = "";
+  }
+  
